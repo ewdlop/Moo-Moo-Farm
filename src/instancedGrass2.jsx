468cda07b3f76af1fd5e4094e190552ae816f9df
ewdlop <ray810815@gmail.com>
2023-08-20 19:42:11 -0400

    Commented out GrassPieceGround

--- src/instancedGrass2.jsx
+++ src/instancedGrass2.jsx
@@ -76,7 +76,7 @@ export default function Grass({ options = { bW: 0.12, bH: 1, joints: 5 }, width 
   useFrame((state) => (materialRef.current.uniforms.time.value = state.clock.elapsedTime / 4))
   return (
     <group {...props}>
-      <GrassPieceGround offsets={attributeData.offsets} />
+      {/* <GrassPieceGround offsets={attributeData.offsets} /> */}
       <mesh>
         <instancedBufferGeometry index={baseGeom.index} attributes-position={baseGeom.attributes.position} attributes-uv={baseGeom.attributes.uv}>
           <instancedBufferAttribute attach="attributes-offset" args={[new Float32Array(attributeData.offsets), 3]} />
