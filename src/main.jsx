ac6e120a66d10a3ac75ccb04a4cd425e5480b423
ewdlop <ray810815@gmail.com>
2023-08-21 21:08:57 -0400

    Remove a bad Skybox import

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import Music from './music/music.jsx'
 import Birds from './sfx/birds.jsx'
 import Wind from './sfx/wind.jsx'
 import SoundComponent from './sfx/soundComponent.jsx'
-import Skybox from './Skybox.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Canvas shadows style={{ width: '100%', height: '100vh' }}>
